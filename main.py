940e5c6d58eca47b517907b48d374c73361247a5
AskMeAgain <20632124+AskMeAgain@users.noreply.github.com>
2021-12-30 22:02:23 +0100

    Adding cli switches

--- main.py
+++ main.py
@@ -1,64 +1,82 @@
+import os
+import sys
 import threading
 import re
 import json
-import os
+import subprocess
+from signal import signal, SIGUSR1
 
 from pathlib import Path
 from typing import Dict
 
-from PIL import Image, ImageDraw, ImageFont
+import psutil
+from PIL import Image
 from StreamDeck.DeviceManager import DeviceManager
+from StreamDeck.Devices import StreamDeck
 from StreamDeck.ImageHelpers import PILHelper
 
 
-state: Dict[str, Dict[str, Dict[str, str]]] = {}
+state: Dict[str, Dict[str, Dict[str, Dict[str, str]]]] = {}
+decks: StreamDeck = []
 
 
-def replace_with_state(deck_id: str, line: str) -> str:
+def replace_with_state(deck_id: str, page: str, line: str) -> str:
     groups = re.search("\\$_state_(.*)_\\$", line)
     if not groups:
         return line
     new_key = groups.group(1)
-    resolved_btn = state[deck_id][new_key]
+    resolved_btn = state[deck_id][page][new_key]
     resolved_state = resolved_btn["internal_state"][resolved_btn["toggle_index"]]
     return line.replace(f"$_state_{new_key}_$", resolved_state)
 
 
-def execute_command(deck_id: str, command: str) -> bool:
-    fixed_command = replace_with_state(deck_id, command)
+def execute_command(deck_id: str, page: str, command: str) -> bool:
+    fixed_command = replace_with_state(deck_id, page, command)
     print(f"Executing command: {fixed_command}")
     return True
 
 
-def toggle(deck_id: str, key: str):
-    if "toggle_index" in state[deck_id][key]:
-        state[deck_id][key]["toggle_index"] += 1
-        state[deck_id][key]["toggle_index"] %= len(state[deck_id][key]["internal_state"])
+def toggle(deck_id: str, page: str, key: str):
+    if "toggle_index" in state[deck_id][page][key]:
+        state[deck_id][page][key]["toggle_index"] += 1
+        state[deck_id][page][key]["toggle_index"] %= len(state[deck_id][page][key]["internal_state"])
 
 
-def button_activated(deck_id: str, key: str):
+def save_file():
+    with open('.streamdeck4p.json', 'w') as f:
+        f.write(json.dumps(state, indent=2))
+
+
+def button_activated(deck_id: str, page: str, key: str):
     command_worked = True
-    if "command" in state[deck_id][key]:
-        command_worked = execute_command(deck_id, state[deck_id][key]["command"])
+    if "command" in state[deck_id][page][key]:
+        command_worked = execute_command(deck_id, page, state[deck_id][page][key]["command"])
     if command_worked:
-        toggle(deck_id, key)
-    render(deck)
+        toggle(deck_id, page, key)
+    if "next_page" in state[deck_id][page][key]:
+        state[deck_id]["current_page"] = state[deck_id][page][key]["next_page"]
+    save_file()
+    render_gui('', '')
 
 
 def key_change_callback(deck, key, button_pressed):
     if button_pressed:
         deck_id = deck.get_serial_number()
-
-        if str(key) in state[deck_id]:
-            button_activated(deck_id, str(key))
+        page = state[deck_id]["current_page"]
+        if str(key) in state[deck_id][page]:
+            button_activated(deck_id, page, str(key))
         else:
             print("no feature!")
 
 
-def load_state():
+def load_state(insert_pid: bool):
     global state
     txt = Path('.streamdeck4p.json').read_text()
     state = json.loads(txt)
+
+    if insert_pid:
+        state["pid"] = os.getpid()
+        save_file()
 
 
 def generate_image(deck, icon_filename) -> Image:
@@ -67,36 +85,67 @@ def generate_image(deck, icon_filename) -> Image:
     return PILHelper.to_native_format(deck, image)
 
 
-def render(deck):
-    for key in range(deck.key_count()):
-        deck_id = deck.get_serial_number()
+def render_gui(a, b):
+    load_state(False)
+    for deck in decks:
+        for key in range(deck.key_count()):
+            deck_id = deck.get_serial_number()
+            deck_state = state[deck_id]
+            page = deck_state["current_page"]
 
-        if str(key) in state[deck_id]:
-            if "image_url" in state[deck_id][str(key)]:
-                img_url = state[deck_id][str(key)]["image_url"]
-                replaced_img = replace_with_state(deck_id, img_url)
+            if page in deck_state and str(key) in deck_state[page] and "image_url" in deck_state[page][str(key)]:
+                img_url = deck_state[page][str(key)]["image_url"]
+                replaced_img = replace_with_state(deck_id, page, img_url)
                 image = generate_image(deck, replaced_img)
                 with deck:
                     deck.set_key_image(key, image)
+            else:
+                with deck:
+                    deck.set_key_image(key, 0x00)
+
+
+def cli_switches() -> bool:
+    if "--switch-page" in sys.argv:
+        load_state(False)
+        deck_id = sys.argv[2]
+        next_page = sys.argv[3]
+        state[deck_id]["current_page"] = str(next_page)
+        save_file()
+        subprocess.run(["kill", "-USR1", str(state["pid"])])
+        return True
+    elif "--show-devices" in sys.argv:
+        streamdecks = DeviceManager().enumerate()
+        for index, deck in enumerate(streamdecks):
+            deck.open()
+            print(f"Found StreamDeck: {deck.get_serial_number()}")
+            deck.close()
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
-    streamdecks = DeviceManager().enumerate()
 
-    print("Found {} Stream Deck(s).\n".format(len(streamdecks)))
+    if not cli_switches():
+        streamdecks = DeviceManager().enumerate()
 
-    for index, deck in enumerate(streamdecks):
-        deck.open()
-        deck.reset()
+        decks = list()
+        load_state(True)
 
-        load_state()
-        render(deck)
+        for index, deck in enumerate(streamdecks):
+            deck.open()
+            deck.reset()
 
-        deck.set_key_callback(key_change_callback)
+            decks.append(deck)
 
-        for t in threading.enumerate():
-            if t is threading.currentThread():
-                continue
+            render_gui("", "")
 
-            if t.is_alive():
-                t.join()
+            deck.set_key_callback(key_change_callback)
+            signal(SIGUSR1, render_gui)
+
+            for t in threading.enumerate():
+                if t is threading.currentThread():
+                    continue
+
+                if t.is_alive():
+                    t.join()
